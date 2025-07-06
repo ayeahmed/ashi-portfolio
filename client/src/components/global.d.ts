871701df71e8e252681d89b1bbc7487f8185baf1
ayeahmed <ayeahmed@umich.edu>
2025-07-05 22:49:38 -0500

    Fix build failures

---
+++ client/src/components/global.d.ts
@@ -0,0 +1,9 @@
+// Tell TypeScript how to handle .css imports
+declare module '*.css';
+
+// If you import images (jpg/png/svg/etc) as modules:
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+declare module '*.gif';
