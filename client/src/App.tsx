8e26dbf921aea01283fa02fc18a8006c405ef79d
ayeahmed <ayeahmed@umich.edu>
2025-07-05 22:41:49 -0500

    Push for deployment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,18 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
 import Experience from './components/Experience';
+import Extras from './components/Extras';
+// import { PopupProvider } from './context/PopupContext';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      {/* 📄 Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/experience" element={<Experience />} />
-      </Routes>
-    </Router>
+    // <PopupProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/extras-about-me" element={<Extras />} />
+        </Routes>
+      </Router>
+    // </PopupProvider>
+    
   );
 }
 
